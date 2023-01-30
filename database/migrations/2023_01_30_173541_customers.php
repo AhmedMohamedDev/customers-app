a90fc17d6dff3e53260a32ea65aaadc9c197863e
Ahmed Mohmed <ahmedmohameddev@gmail.com>
2023-01-30 19:45:09 +0200

    app init

---
+++ database/migrations/2023_01_30_173541_customers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname',100);
+            $table->string('lastname',100);
+            $table->string('user_name',100)->unique();
+            $table->string('email',100)->unique();
+            $table->float('salary', 8, 2);
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
